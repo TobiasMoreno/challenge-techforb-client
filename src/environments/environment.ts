3663508a074db6d81453ede790c8187033070fe8
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2025-02-14 23:06:43 -0300

    fix: update production API URL for environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://challenge-techforb-server-production.up.railway.app/',
+  apiUrl: 'https://challenge-techforb-server-production-50d9.up.railway.app/api',
 };
